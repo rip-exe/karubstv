950ebdc0a56687d54e2e1f64ca990db73f1d9e23
rip-exe <74199108+rip-exe@users.noreply.github.com>
2021-05-29 21:44:21 +0300

    Add files via upload

---
+++ başlat.bat
@@ -0,0 +1,6 @@
+echo off
+color b
+cls
+:a
+node main.js
+goto a
---
+++ bot.js
@@ -0,0 +1,7 @@
+const client = global.client;
+
+client.on("ready", () => {
+    console.log("Hilal ❤️ Burak");
+});
+
+client.login("ODQ0NDg4NjA5NTExMjQzNzg2.YKTJTg.hu4f2RytVErdwoJDzBRhol-F6tg");
